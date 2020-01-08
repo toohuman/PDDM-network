75c0ef1845630bc253876085ceb8c23eeb14f530
toohuman <michael@toohuman.io>
2020-01-08 11:18:49 +0000

    Plotting file working for heatmaps.

--- plotting/plot_hm_loss_connectivity_evidence.py
+++ plotting/plot_hm_loss_connectivity_evidence.py
@@ -7,50 +7,57 @@ PERC_LOWER = 10
 PERC_UPPER = 90
 
 agents_set = [100]
-states_set = [5, 10, 20, 50, 100]
-states_set_formatted = ["5x5", "10x10", "20x20", "50x50", "100x100"]
+states_set = [10]
 evidence_rates = [0.0, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0]
+evidence_strings = ["{:.3f}".format(x) for x in evidence_rates]
 noise_values = [0.0, 1.0, 5.0, 10.0, 20.0, 100.0]
 connectivity_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+connectivity_strings = ["{:.1f}".format(x) for x in connectivity_values]
 
 result_directory = "../../results/test_results/pddm-network/"
 
-for j, agents in enumerate(agents_set):
-    heatmap_results = np.array([[0.0 for x in noise_values] for y in states_set])
-    labels = [["" for x in noise_values] for y in states_set]
-
-    skip = True
-
-    for i, states in enumerate(reversed(states_set)):
+for a, agents in enumerate(agents_set):
+    for s, states in enumerate(states_set):
         for n, noise in enumerate(noise_values):
-            file_name_parts = ["loss", agents, "agents", states, "states", "{:.3f}".format(er), "er", "{:.3f}".format(noise), "nv"]
-            file_ext = ".csv"
-            file_name = "_".join(map(lambda x: str(x), file_name_parts)) + file_ext
 
-            steady_state_results = []
+            heatmap_results = np.array([[0.0 for x in connectivity_values] for y in evidence_rates])
+            labels = [["" for x in connectivity_values] for y in evidence_rates]
 
-            labels[i][n] = "{}:{}".format(states, noise)
+            skip = True
 
-            try:
-                with open(result_directory + file_name, "r") as file:
-                    for line in file:
-                        steady_state_results = line
-                print(file_name)
+            for e, er in enumerate(reversed(evidence_rates)):
+                for c, con in enumerate(connectivity_values):
 
-                steady_state_results = [float(x) for x in steady_state_results.strip().split(",")]
+                    file_name_parts = ["loss", agents, "agents", states, "states", "{:.3f}".format(noise), "nv", "{:.3f}".format(er), "er", "{:.1f}".format(con), "con"]
+                    file_ext = ".csv"
+                    file_name = "_".join(map(lambda x: str(x), file_name_parts)) + file_ext
+                    print(file_name)
 
-                average_loss = np.average(steady_state_results)
+                    labels[s][n] = "{} A : {} S : {} N".format(agents, states, noise)
 
-                heatmap_results[i][n] = average_loss
+                    steady_state_results = []
+                    average_loss = 0.0
 
-                skip = False
+                    try:
+                        with open(result_directory + file_name, "r") as file:
+                            for line in file:
+                                steady_state_results = line
+                        print(file_name)
 
-            except FileNotFoundError:
-                # Add obvious missing entry into final results array here
-                heatmap_results[i][n] = 1.0
+                        steady_state_results = [float(x) for x in steady_state_results.strip().split(",")]
 
-    if skip:
-        continue
+                        average_loss = np.average(steady_state_results)
+
+                        heatmap_results[s][n] = average_loss
+
+                        skip = False
+
+                    except FileNotFoundError:
+                        # Add obvious missing entry into final results array here
+                        heatmap_results[s][n] = 1.0
+
+            if skip:
+                continue
 
     print(heatmap_results)
     print(labels)
@@ -61,7 +68,7 @@ for j, agents in enumerate(agents_set):
         cmap=cmap,
         cbar_kws={"shrink": .75},
         xticklabels=noise_values,
-        yticklabels=list(reversed(states_set_formatted)),
+        yticklabels=list(reversed(evidence_strings)),
         vmin=0, vmax=1,
         linewidths=.5,
         annot=True,
@@ -69,8 +76,8 @@ for j, agents in enumerate(agents_set):
         fmt=".2f",
         square=True
     )
-    plt.title("Average loss | {} agents, {} evidence rate".format(agents, er))
-    ax.set(xlabel='Noise value', ylabel='No. of States')
+    plt.title("Average loss | {} agents, {} states, {} noise".format(agents, states, noise))
+    ax.set(xlabel='Connectivity', ylabel='Evidence rate')
     # plt.show()
-    plt.savefig("../../results/graphs/pddm-network/loss_hm_{}_agents_{}-{}_states_noisy{}{}.pdf".format(agents, states_set[0], states_set[-1], partition_str, exploration_str), bbox_inches="tight")
+    plt.savefig("../../results/graphs/pddm-network/hm_loss_{}_agents_{}_states_{}_noise_er_con.pdf".format(agents, states, noise), bbox_inches="tight")
     plt.clf()
